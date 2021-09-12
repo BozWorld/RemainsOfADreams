cc747850dc3657ff05793c2428fd74a55fe2b12e
joseph pelloquet <joseph.pelloquet@gmail.com>
2021-09-12 13:30:38 +0200

    push

--- Assets/Script/BallBahevior.cs
+++ Assets/Script/BallBahevior.cs
@@ -9,17 +9,17 @@ public class BallBahevior : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        Debug.Log("Collision!");
+        
         if (collision.gameObject.tag == "Player")
         {
             forceApplied = 50;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * forceApplied);
         }
-        
+        /*
         if (collision.gameObject.tag == "Halte")
         {
             forceApplied = 0;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * forceApplied);
-        }
+        }*/
     }
 }
